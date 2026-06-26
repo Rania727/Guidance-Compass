9cac4cf557412b11b544ec74a3bfcce82a7f5352
Rania727 <raniaahmm527@gmail.com>
2026-06-26 14:37:09 -0700

    Create build.mjs

---
+++ backend/build.mjs
@@ -0,0 +1,18 @@
+import { build } from "esbuild";
+import { rm } from "fs/promises";
+
+await rm("dist", { recursive: true, force: true });
+
+await build({
+  entryPoints: ["src/index.ts"],
+  platform: "node",
+  bundle: true,
+  format: "cjs",
+  outfile: "dist/index.cjs",
+  define: { "process.env.NODE_ENV": '"production"' },
+  minify: true,
+  external: ["pg-native"],
+  logLevel: "info",
+});
+
+console.log("Build complete!");
